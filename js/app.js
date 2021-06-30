39622ef69baddfa6ab4a98c323a7b3e6eb0608ae
RileyCC56 <rileycorpac@gmail.com>
2021-06-30 17:56:15 -0400

    app.js update

--- js/app.js
+++ js/app.js
@@ -20,7 +20,30 @@ data.forEach((dataRow) => {
         }   
     );
 });
-//(Function ABV 13-22. Looped through each object in the array
+//(Function from lines 13-22. Looped through each object in the array
 //Appended a row to the HTML table
 //Added each value from the object into a cell
 
+function handleClick() {
+     // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+     // Check to see if a date was entered and filter the
+  // data using that date.
+    if (date) {
+          // Apply `filter` to the table data to only keep the
+    // rows where the `datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+      };
+  // Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will
+  // just be the original tableData.
+
+    buildTable(filteredData);
+
+};
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
